6ec69c2c9b8f2a4c8c5f4b119774406000025bba
Zaquariah Holland <zaquariah@gmail.com>
2021-02-04 21:51:24 -0600

    updated BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from "react-router-dom";
+// import { Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 import App from './App';
@@ -12,9 +13,9 @@ import './assets/scss/style.scss';
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
+  <BrowserRouter history={history} basename="/officer-elections">
     <App />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
