ee71556c2b2da169d489699f6c2a8e486276058a
Zaquariah Holland <zaquariah@gmail.com>
2021-02-04 18:36:34 -0600

    Updates

---
+++ precache-manifest.970ea308f1c961da7f4fe8cf8ff31771.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36304c184d34c3d66a3263a3d2526548",
+    "url": "/officer-elections/index.html"
+  },
+  {
+    "revision": "a006b852ee4304a32e90",
+    "url": "/officer-elections/static/css/main.760155b8.chunk.css"
+  },
+  {
+    "revision": "2b91a7709c6a2b212d5c",
+    "url": "/officer-elections/static/js/2.b845edf2.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/officer-elections/static/js/2.b845edf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a006b852ee4304a32e90",
+    "url": "/officer-elections/static/js/main.03ff0d31.chunk.js"
+  },
+  {
+    "revision": "79d05509776fd08abdb9",
+    "url": "/officer-elections/static/js/runtime-main.6b1adc5c.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/officer-elections/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "b7a9d36a38139677471141f20ab42b62",
+    "url": "/officer-elections/static/media/feature-acm-wiki.b7a9d36a.png"
+  },
+  {
+    "revision": "975539bb2a036f37aedf6353326413ea",
+    "url": "/officer-elections/static/media/feature-roadrunner-cycling.975539bb.jpg"
+  },
+  {
+    "revision": "2e478bc1f4b8e428d5e548890dd7cb4c",
+    "url": "/officer-elections/static/media/feature-rowdyhacks.2e478bc1.png"
+  },
+  {
+    "revision": "a520cf524e9cb7d10012d64916ec5e01",
+    "url": "/officer-elections/static/media/feature-tile-icon-01.a520cf52.svg"
+  },
+  {
+    "revision": "5111bcd5cbd9a25e7c2e8c017eeb3eb2",
+    "url": "/officer-elections/static/media/feature-tile-icon-02.5111bcd5.svg"
+  },
+  {
+    "revision": "198608f5e4889b441864d8be6843cd09",
+    "url": "/officer-elections/static/media/feature-tile-icon-03.198608f5.svg"
+  },
+  {
+    "revision": "dc69deddc05b57617f990411d930243f",
+    "url": "/officer-elections/static/media/feature-tile-icon-04.dc69dedd.svg"
+  },
+  {
+    "revision": "af818383a38fe7a06acead98d9d17d72",
+    "url": "/officer-elections/static/media/feature-tile-icon-06.af818383.svg"
+  },
+  {
+    "revision": "2b0c37c3fa22e3ce5c531c65c5aa0e7e",
+    "url": "/officer-elections/static/media/illustration-section-01.2b0c37c3.svg"
+  },
+  {
+    "revision": "7593fba705de9ae7b533fa9b61633839",
+    "url": "/officer-elections/static/media/illustration-section-02.7593fba7.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/officer-elections/static/media/video-placeholder.a622fc5d.jpg"
+  },
+  {
+    "revision": "4359350e29ca89b8c812351a437d7bd1",
+    "url": "/officer-elections/static/media/z-logo.4359350e.svg"
+  }
+]);
