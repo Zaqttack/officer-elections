2b972dcff32334a087887fd086dc383c55287674
Zaquariah Holland <zaquariah@gmail.com>
2021-02-04 22:04:22 -0600

    Updates

---
+++ precache-manifest.3f93479748f86819752826457215bb95.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fec25ee2c175439507fcd0272e37d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1692cdf3cb953b01d69b",
+    "url": "/static/css/main.6dc9df03.chunk.css"
+  },
+  {
+    "revision": "02ba6186206f31214fe8",
+    "url": "/static/js/2.d640c1bb.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.d640c1bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1692cdf3cb953b01d69b",
+    "url": "/static/js/main.f9d23fd9.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "b7a9d36a38139677471141f20ab42b62",
+    "url": "/static/media/feature-acm-wiki.b7a9d36a.png"
+  },
+  {
+    "revision": "975539bb2a036f37aedf6353326413ea",
+    "url": "/static/media/feature-roadrunner-cycling.975539bb.jpg"
+  },
+  {
+    "revision": "2e478bc1f4b8e428d5e548890dd7cb4c",
+    "url": "/static/media/feature-rowdyhacks.2e478bc1.png"
+  },
+  {
+    "revision": "a520cf524e9cb7d10012d64916ec5e01",
+    "url": "/static/media/feature-tile-icon-01.a520cf52.svg"
+  },
+  {
+    "revision": "5111bcd5cbd9a25e7c2e8c017eeb3eb2",
+    "url": "/static/media/feature-tile-icon-02.5111bcd5.svg"
+  },
+  {
+    "revision": "198608f5e4889b441864d8be6843cd09",
+    "url": "/static/media/feature-tile-icon-03.198608f5.svg"
+  },
+  {
+    "revision": "dc69deddc05b57617f990411d930243f",
+    "url": "/static/media/feature-tile-icon-04.dc69dedd.svg"
+  },
+  {
+    "revision": "af818383a38fe7a06acead98d9d17d72",
+    "url": "/static/media/feature-tile-icon-06.af818383.svg"
+  },
+  {
+    "revision": "2b0c37c3fa22e3ce5c531c65c5aa0e7e",
+    "url": "/static/media/illustration-section-01.2b0c37c3.svg"
+  },
+  {
+    "revision": "7593fba705de9ae7b533fa9b61633839",
+    "url": "/static/media/illustration-section-02.7593fba7.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/static/media/video-placeholder.a622fc5d.jpg"
+  },
+  {
+    "revision": "4359350e29ca89b8c812351a437d7bd1",
+    "url": "/static/media/z-logo.4359350e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/officer-elections/precache-manifest.de3f437159a04720b29d3580870015e9.js"
+  "/precache-manifest.3f93479748f86819752826457215bb95.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/officer-elections/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
