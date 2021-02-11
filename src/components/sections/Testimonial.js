6519000e6c56d0e27f47c2f99a644f2b2a6ab883
Zaquariah Holland <zaquariah@gmail.com>
2021-02-11 16:15:07 -0600

    updated: final testimonial

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -113,8 +113,7 @@ const Testimonial = ({
               <div className="tiles-item-inner">
                 <div className="testimonial-item-content">
                   <p className="text-sm mb-0">
-                    — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
-                  </p>
+                    — ACM is full of great people but I've found myself getting closest to Zaq. He's really chill, friendly and helpful. Anyone in ACM would agree that he's a great contribution to the team, and would make as an amazing President. Anyone would be lucky to have him as a friend! Zaq = G.O.A.T.</p>
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
                   <span className="testimonial-item-name text-color-high">Desiree Garcia</span>
